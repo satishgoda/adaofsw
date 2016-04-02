d7ed3bba1f2e2ea1ba1cff80f55f9ee897f59363
Satish Goda <satishgoda@live.com>
2016-04-03 02:18:35 +0800

    Tool and ToolManager prototype : Tool history

--- sandbox/app/toolmgr/proto.py
+++ sandbox/app/toolmgr/proto.py
@@ -1,12 +1,11 @@
 from collections import OrderedDict
 
-
 class _ToolManager(object):
     def __init__(self):
         self.tools = OrderedDict()
     
     def addTool(self, tool):
-        self.tools[tool.name] = tool
+        self.tools[tool.name] = {'instance': tool, 'history': []}
     
     def invokeTool(self, tool):
         print("\nUser invoked tool '{0}'\nAsking user to input values for tool properties".format(tool.name))
@@ -19,6 +18,8 @@ class _ToolManager(object):
                 return
             else:
                 values.append(prop_value)
+        
+        self.tools[tool.name]['history'].append(values)
         
         self.executeTool(tool, values)
     
@@ -46,26 +47,43 @@ class Tool(object):
         for prop, value in zip(self.properties, values):
             print("\t '{0} -> {1}'".format(prop, value))
 
-tool1 = Tool("Create Sphere", "name, radius")
-tool2 = Tool("Create Cylinder", "name, radius, height")
+createSphere = Tool("Create Sphere", "name, radius")
+createCylinder = Tool("Create Cylinder", "name, radius, height")
 
 print ToolManager.tools
 
-tool1.invoke()
+createSphere.invoke()
 
-tool2.invoke()
+createSphere.invoke()
 
-tool1.execute(('sphere2', 3))
+print ToolManager.tools[createSphere.name]['history']
+
+createCylinder.invoke()
+
+print ToolManager.tools[createCylinder.name]['history']
+
+createSphere.execute(('sphere2', 3))
+
+createCylinder.execute(('cylinder20', 3, 6))
+
 
 """
-OrderedDict([('Create Sphere', <__main__.Tool object at 0x00000007B07A3EB8>), ('Create Cylinder', <__main__.Tool object at 0x00000007B07A3E80>)])
+OrderedDict([('Create Sphere', {'instance': <__main__.Tool object at 0x00000007E9128E80>, 'history': []}), ('Create Cylinder', {'instance': <__main__.Tool object at 0x00000007E9128F98>, 'history': []})])
 
 User invoked tool 'Create Sphere'
 Asking user to input values for tool properties
 name,radius
 Create Sphere with
-	 'name, -> sphere1'
-	 'radius -> 3'
+	 'name, -> sph1'
+	 'radius -> 2'
+
+User invoked tool 'Create Sphere'
+Asking user to input values for tool properties
+name,radius
+Create Sphere with
+	 'name, -> sph2'
+	 'radius -> 4'
+[[u'sph1', u'2'], [u'sph2', u'4']]
 
 User invoked tool 'Create Cylinder'
 Asking user to input values for tool properties
@@ -73,9 +91,15 @@ name,radius,height
 Create Cylinder with
 	 'name, -> cyl1'
 	 'radius, -> 2'
-	 'height -> 4'
+	 'height -> 3'
+[[u'cyl1', u'2', u'3']]
 
 Create Sphere with
 	 'name, -> sphere2'
 	 'radius -> 3'
+
+Create Cylinder with
+	 'name, -> cylinder20'
+	 'radius, -> 3'
+	 'height -> 6'
 """
