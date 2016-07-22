fa7183d48820ff7acde9e8c4ecc5047d95c8cf91
Satish Goda <satishgoda@live.com>
2016-07-22 18:32:02 +0800

    Create editors.py

---
+++ sandbox/app/pkr/ui/editors.py
@@ -0,0 +1,14 @@
+class ItemEditor(QWidget):
+    pass
+
+class ItemDisplayEditor(ItemEditor):
+    pass
+
+class ItemContentEditor(ItemEditor):
+    pass
+
+class ItemControlContentEditor(ItemContentEditor):
+    pass
+
+class ItemScriptContentEditor(ItemContentEditor):
+    pass
