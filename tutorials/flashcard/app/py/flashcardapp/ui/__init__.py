e34e9e4c1b74ef810de467572a3d5618742398d7
Satish Goda <satishgoda@live.com>
2017-12-30 01:53:42 +0800

    Hook up the DeckReview class into the UI

--- tutorials/flashcard/app/py/flashcardapp/ui/__init__.py
+++ tutorials/flashcard/app/py/flashcardapp/ui/__init__.py
@@ -1,7 +1,7 @@
 from PyQt4 import QtGui
 from PyQt4 import QtCore
 
-from flashcardapp.deck import Deck
+from flashcardapp.deck import Deck, DeckReview
 from flashcardapp.card import Card
 
 deck_review_widget = QtGui.QWidget()
@@ -12,7 +12,15 @@ mainLayout = QtGui.QVBoxLayout()
 mainLayout.setAlignment(QtCore.Qt.AlignJustify)
 deck_review_widget.setLayout(mainLayout)
 
-card_view = QtGui.QLabel("Undefined deck")
+deck = Deck("Test1")
+
+for front, back in [('a', 'A'), ('and', 'AND'), ('can', 'CAN')]:
+    card = Card(front, back)
+    deck.add_card(card)
+
+review = DeckReview(deck)
+
+card_view = QtGui.QLabel(review.current_card.front)
 
 font = card_view.font()
 font.setPointSize(60)
@@ -32,3 +40,16 @@ next_button = QtGui.QPushButton("next")
 
 buttonsLayout.addWidget(prev_button)
 buttonsLayout.addWidget(next_button)
+
+def next_card():
+    card = review.next_card
+    card_view.setText(card.front)
+
+def prev_card():
+    card = review.previous_card
+    card_view.setText(card.front)
+
+prev_button.clicked.connect(prev_card)
+next_button.clicked.connect(next_card)
+
+deck_review_widget.adjustSize()
